d46e02f403e0bd7bbfbb4e8376cce1c6ed831603
Plora Tran <ploratran@gmail.com>
2020-12-30 18:15:32 -0800

    implement Page Object for login and signup

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/pages/LoginPage.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/pages/LoginPage.java
@@ -1,4 +1,34 @@
 package com.udacity.jwdnd.course1.cloudstorage.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+// Page Object of login.html:
 public class LoginPage {
+
+    // define fields:
+    @FindBy(id = "inputUsername")
+    private WebElement username;
+
+    @FindBy(id = "inputPassword")
+    private WebElement password;
+
+    @FindBy(tagName = "button")
+    private WebElement submitBtn;
+
+    // constructor:
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    // methods:
+    public void login(String username, String password) {
+        // field in data:
+        this.username.sendKeys("ploratran");
+        this.password.sendKeys("p@ssword");
+        // hit Login button:
+        this.submitBtn.click();
+    }
 }
--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/pages/SignupPage.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/pages/SignupPage.java
@@ -1,4 +1,42 @@
 package com.udacity.jwdnd.course1.cloudstorage.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+// define Page Object for signup.html:
 public class SignupPage {
+
+    // define fields:
+    @FindBy(id = "inputFirstName")
+    private WebElement firstname;
+
+    @FindBy(id = "inputLastName")
+    private WebElement lastname;
+
+    @FindBy(id = "inputUsername")
+    private WebElement username;
+
+    @FindBy(id = "inputPassword")
+    private WebElement password;
+
+    @FindBy(tagName = "button")
+    private WebElement signupBtn;
+
+    // constructor:
+    public SignupPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    // methods:
+    public void signup(String fname, String lname, String username, String password) {
+        // field data:
+        this.firstname.sendKeys(fname);
+        this.lastname.sendKeys(lname);
+        this.username.sendKeys(username);
+        this.password.sendKeys(password);
+        // hit Sign Up button:
+        this.signupBtn.click();
+    }
 }
