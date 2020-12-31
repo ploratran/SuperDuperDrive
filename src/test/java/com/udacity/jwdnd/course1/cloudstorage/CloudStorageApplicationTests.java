3a1d5bdc7a9df3cf456b8309778014ce1a2315e8
Plora Tran <ploratran@gmail.com>
2020-12-30 18:36:45 -0800

    finish implement signup/login test to simulate user auth process

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -1,5 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
+import com.udacity.jwdnd.course1.cloudstorage.pages.LoginPage;
+import com.udacity.jwdnd.course1.cloudstorage.pages.SignupPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
@@ -11,31 +13,58 @@ import org.springframework.boot.web.server.LocalServerPort;
 class CloudStorageApplicationTests {
 
 	@LocalServerPort
-	private int port;
+	private int port; // RANDOM_PORT, port of server
 
-	private WebDriver driver;
+	// fields:
+	private static WebDriver driver;
+
+	private SignupPage signupPage;
+	private LoginPage loginPage;
+
+	String baseURL;
 
 	@BeforeAll
-	static void beforeAll() {
+	public static void beforeAll() {
 		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+
+	@AfterAll
+	public void afterAll() {
+		driver.quit();
 	}
 
 	@BeforeEach
 	public void beforeEach() {
-		this.driver = new ChromeDriver();
-	}
-
-	@AfterEach
-	public void afterEach() {
-		if (this.driver != null) {
-			driver.quit();
-		}
+		baseURL = "http://localhost:" + port;
 	}
 
 	@Test
-	public void getLoginPage() {
-		driver.get("http://localhost:" + this.port + "/login");
-		Assertions.assertEquals("Login", driver.getTitle());
+	public void testAuthorization() {
+		// define data to fill in:
+		String firstname = "Phuong";
+		String lastname = "Tran";
+		String username = "ploratran";
+		String password = "p@ssword";
+
+		// navigate to /signup:
+		driver.get(baseURL + "/signup");
+
+		// initialize object of SignupPage
+		// call .signup() to simulate user's signup:
+		signupPage = new SignupPage(driver);
+		signupPage.signup(firstname, lastname, username, password);
+
+		// after signup, navigate to /login:
+		driver.get(baseURL + "/login");
+
+		// initialize object of LoginPage
+		// call .login() to simulate user's login:
+		loginPage = new LoginPage(driver);
+		loginPage.login(username, password);
+
+		// after successfully login, auto navigate to /home:
+
 	}
 
 }
