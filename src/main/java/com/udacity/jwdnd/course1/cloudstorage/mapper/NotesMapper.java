a425137cd432ebc0f33c6f44d60b7cff88247b18
Plora Tran <ploratran@gmail.com>
2021-01-02 22:00:38 -0800

    fix error adding note. Use Note model to map to the right database properties on MyBatis NotesMapper.java

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NotesMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NotesMapper.java
@@ -8,17 +8,23 @@ import java.util.List;
 @Mapper
 public interface NotesMapper {
     // GET all notes from Notes db by userId:
+    @Results({
+            @Result(id = true, property = "noteId", column = "noteid"),
+            @Result(property = "title", column = "notetitle"),
+            @Result(property = "description", column = "notedescription"),
+            @Result(property = "userId", column = "userid")
+    })
     @Select("SELECT * FROM NOTES WHERE userId = #{userId}")
     List<Note> findNotesByUser(int userId);
 
     // INSERT/ADD new note into Notes db by specific noteId:
     @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) " +
-    "VALUES(#{noteTitle}, #{noteDescription}, #{userId})")
+    "VALUES(#{title}, #{description}, #{userId})")
     @Options(useGeneratedKeys = true, keyProperty = "noteId")
     int addNoteByNoteId(Note note);
 
     // EDIT/UPDATE note title and note description by noteId:
-    @Update("UPDATE NOTES SET notetitle=#{noteTitle}, notedescription=#{noteDescription}" +
+    @Update("UPDATE NOTES SET notetitle=#{title}, notedescription=#{description}" +
     "WHERE noteid=#{noteId}")
     int updateNoteById(Note note);
 
